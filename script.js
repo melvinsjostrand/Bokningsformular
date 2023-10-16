61c34eb2fb1ab096cf18f2f2b8cf6079d99a16d4
melvinsjostrand <mesj04001@utb.vaxjo.se>
2023-10-16 20:59:12 +0200

    nummer och postnummer

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let roomType;
 let lakeview;
 let form; 
 let addition;
+let customer;
+
 function init(){
     form = document.querySelector("form");
     let formCost = [form.elements.roomType && 
@@ -22,10 +24,16 @@ function init(){
             cost();
         });
     }
-    form.elements.telephone.addEventListener("click" , event =>{
+    form.elements.telephone.addEventListener("input" , event =>{
         phoneNumber();
     })
 
+    form.elements.zipcode.addEventListener("input" , event =>{
+        zipCode();
+    })
+    form.elements.campaigncode.addEventListener("input" , event =>{
+        campaignCode();
+    })
     checkIfFamilyRoom();
 
 }
@@ -52,7 +60,7 @@ function cost(){
     let roomCost = Number(form.elements.roomType.value.split(",")[1]);
     console.log(roomCost);
     let totalCost = document.getElementById("totalCost");
-    totalCost.innerHTML = (roomCost * nights);
+   // totalCost.innerHTML = (roomCost * nights) + (additionCost()*nights) * campaigncode();
     console.log(roomCost * nights);
 }
 //skapa en till if else sats för att vilka som är i checkade
@@ -70,13 +78,13 @@ function additionCost(){
 
 
 function phoneNumber(){
-    const regex = /^0[0-9-/ ]{6,14}$/;
-    let customer = document.getElementById("customer");
-    let phone = Number(form.elements.telephone.value);
-        customer.getElementsByTagName("p")[4].getElementsByTagName("span")[1];
-    let ok = regex.exec(phone);
+    const regexPhone = /^0[0-9-/ ]{6,14}$/;
+    console.log(regexPhone);
+    customer = document.getElementById("customer");
+    let phone = form.elements.telephone.value;
+    let ok = regexPhone.exec(phone);
     if(!ok){
-        customer.getElementsByTagName("p")[4].getElementsByTagName("span")[1].innerHTML = "Det är fel";
+        customer.getElementsByTagName("p")[4].getElementsByTagName("span")[1].innerHTML = "Du måste skriva 6 till 14 tecken och börja med 0";
         customer.getElementsByTagName("p")[4].getElementsByTagName("span")[1].parentNode.style.color = "#ff0000"; 
         console.log(ok);
     }else{
@@ -84,5 +92,38 @@ function phoneNumber(){
         console.log(ok);
         return ok;
 
+    }
+}
+
+function zipCode(){
+    const regexZip = /[0-9]$/;
+    console.log(regexZip);
+    customer = document.getElementById("customer");
+    let zip = form.elements.zipcode.value;
+    let ok = regexZip.exec(zip);
+    if(!ok){
+        customer.getElementsByTagName("p")[2].getElementsByTagName("span")[1].innerHTML = "Ditt postnummer måste får endast innehålla siffror och har 5 siffror";
+        customer.getElementsByTagName("p")[2].getElementsByTagName("span")[1].parentNode.style.color = "#ff0000"; 
+        console.log(ok);
+    }else{
+        customer.getElementsByTagName("p")[2].getElementsByTagName("span")[1].innerHTML = "";
+        console.log(ok);
+        return ok;
+
+    }
+}
+
+function campaignCode(){
+    const regexCampaign = /[A-Za-z]{3}-[0-9]{2}-[A-Za-z]{1}[0-9]{1}$/;
+    console.log(regexCampaign);
+    let campaignCost = document.getElementById("campaign");
+    let campaign = campaignCost.form.elements.campaigncode.value
+    console.log(campaign);
+    let ok = regexCampaign.exec(campaign);
+    console.log(ok);
+    if(!ok){
+        campaign.parentNode.style.backgroundColor  = "#008000";
+    }else{
+        campaign.parentNode.style.backgroundColor  = "#008000";
     }
 }
