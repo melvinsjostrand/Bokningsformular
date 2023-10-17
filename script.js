57c7470426ac1e3531b86a7a4cd8b3acc73b39ed
melvinsjostrand <mesj04001@utb.vaxjo.se>
2023-10-17 15:01:56 +0200

    hela uppgiften

--- script.js
+++ script.js
@@ -3,39 +3,44 @@
 let roomType;
 let lakeview;
 let form; 
-let addition;
 let customer;
+let campaignCodeValue = 1;
 
 function init(){
     form = document.querySelector("form");
-    let formCost = [form.elements.roomType && 
-                    form.elements.addition && 
-                    form.elements.nights && 
-                    form.elements.campaigncode];
     lakeview = form.elements.addition[2];
+    console.log(form);
 
     for(i = 0; i<form.elements.roomType.length; i++){
         form.elements.roomType[i].addEventListener("click" , event =>{
             checkIfFamilyRoom();
+            cost();
         })
     }
-    for(i = 0; i<formCost.length; i++){
-        formCost[i].addEventListener("click" ,event => {
+    for(i = 0; i<form.elements.addition.length; i++){
+        form.elements.addition[i].addEventListener("click" , event =>{
+            checkIfFamilyRoom();
             cost();
-        });
+        })
     }
-    form.elements.telephone.addEventListener("input" , event =>{
+    form.elements.nights.addEventListener("click" , event =>{
+        cost();
+    })
+    form.elements.campaigncode.addEventListener("keyup" , event =>{
+        campaignCode();
+        cost();
+    })
+
+    form.elements.telephone.addEventListener("blur" , event =>{
         phoneNumber();
     })
-
-    form.elements.zipcode.addEventListener("input" , event =>{
+    form.elements.zipcode.addEventListener("blur" , event =>{
         zipCode();
     })
-    form.elements.campaigncode.addEventListener("input" , event =>{
-        campaignCode();
+    form.elements.city.addEventListener("blur" , event =>{
+        locality();
     })
     checkIfFamilyRoom();
-
 }
 window.onload = init;
 
@@ -43,7 +48,7 @@ window.onload = init;
 function checkIfFamilyRoom(){
     let persons = form.elements.persons;
     if (form.elements.roomType[2].checked === true) {
-        lakeview.disabled = true;
+        lakeview.disabled = true;  
         lakeview.parentNode.style.color = "#000";
         persons.disabled = false;
     }else{
@@ -53,15 +58,17 @@ function checkIfFamilyRoom(){
     }
 }
 
-
-
 function cost(){
     let nights = Number(form.elements.nights.value);
     let roomCost = Number(form.elements.roomType.value.split(",")[1]);
-    console.log(roomCost);
     let totalCost = document.getElementById("totalCost");
-   // totalCost.innerHTML = (roomCost * nights) + (additionCost()*nights) * campaigncode();
-    console.log(roomCost * nights);
+    for(i = 0; i < form.elements.addition.length; i++){
+        if(form.elements.addition[i].checked && !form.elements.addition[i].disabled){
+            roomCost+=Number(form.elements.addition[i].value.split(",")[1]);
+        }
+    }
+    totalCost.innerHTML = (roomCost * nights)  * campaignCodeValue;
+    console.log("värdet på allt är " + (roomCost * nights)  * campaignCodeValue + "Kr");
 }
 //skapa en till if else sats för att vilka som är i checkade
 function additionCost(){
@@ -75,55 +82,48 @@ function additionCost(){
     }
 }
 
-
-
 function phoneNumber(){
     const regexPhone = /^0[0-9-/ ]{6,14}$/;
-    console.log(regexPhone);
-    customer = document.getElementById("customer");
-    let phone = form.elements.telephone.value;
-    let ok = regexPhone.exec(phone);
+    customer = form.elements.telephone.parentNode.parentNode.getElementsByTagName("span")[1];
+    let ok = regexPhone.exec(form.elements.telephone.value);
     if(!ok){
-        customer.getElementsByTagName("p")[4].getElementsByTagName("span")[1].innerHTML = "Du måste skriva 6 till 14 tecken och börja med 0";
-        customer.getElementsByTagName("p")[4].getElementsByTagName("span")[1].parentNode.style.color = "#ff0000"; 
-        console.log(ok);
+        customer.innerHTML = "Du måste skriva 6 till 14 tecken och börja med 0";
+        customer.parentNode.style.color = "#ff0000"; 
     }else{
-        customer.getElementsByTagName("p")[4].getElementsByTagName("span")[1].innerHTML = "";
-        console.log(ok);
-        return ok;
-
+        customer.innerHTML = "";
     }
 }
 
 function zipCode(){
-    const regexZip = /[0-9]$/;
-    console.log(regexZip);
-    customer = document.getElementById("customer");
-    let zip = form.elements.zipcode.value;
-    let ok = regexZip.exec(zip);
+    const regexZip = /[0-9]{5}$/;
+    customer = form.elements.zipcode.parentNode.parentNode.getElementsByTagName("span")[1];
+    let ok = regexZip.exec(form.elements.zipcode.value);
+   
     if(!ok){
-        customer.getElementsByTagName("p")[2].getElementsByTagName("span")[1].innerHTML = "Ditt postnummer måste får endast innehålla siffror och har 5 siffror";
-        customer.getElementsByTagName("p")[2].getElementsByTagName("span")[1].parentNode.style.color = "#ff0000"; 
-        console.log(ok);
+        customer.innerHTML = "Ditt postnummer måste får endast innehålla siffror och har 5 siffror";
+        customer.parentNode.style.color = "#ff0000"; 
     }else{
-        customer.getElementsByTagName("p")[2].getElementsByTagName("span")[1].innerHTML = "";
-        console.log(ok);
-        return ok;
-
+        customer.innerHTML = "";
     }
 }
-
+//funktioner för validering av input fält   
 function campaignCode(){
-    const regexCampaign = /[A-Za-z]{3}-[0-9]{2}-[A-Za-z]{1}[0-9]{1}$/;
-    console.log(regexCampaign);
-    let campaignCost = document.getElementById("campaign");
-    let campaign = campaignCost.form.elements.campaigncode.value
-    console.log(campaign);
-    let ok = regexCampaign.exec(campaign);
-    console.log(ok);
-    if(!ok){
-        campaignCost.form.elements.campaigncode.parentNode.style.backgroundColor  = "#ff0000";
+    const regexCampaign = /[A-Za-z]{3}-[0-9]{2}-[A-Za-z]{1}[0-9]{1}$/; //alk-13-c5, spelar ingen roll om de är stora eller små
+    let ok = regexCampaign.exec(form.elements.campaigncode.value);
+    if(ok){
+        form.elements.campaigncode.style.backgroundColor  = "#99ff99";
+        campaignCodeValue = 0.95;
     }else{
-        campaignCost.parentNode.style.backgroundColor  = "#008000";
+        form.elements.campaigncode.style.backgroundColor  = "#ff0000";
+        campaignCodeValue = 1;
     }
 }
+
+function locality(){
+    let text = form.elements.city.value;
+    text = text.toUpperCase();
+    form.elements.city.value = text;
+}
+
+//behöver skapa if(ok){kampanjkod = -5% = *0.95} i calc
+//måste fixa så att färgen ändras på rätt plats
