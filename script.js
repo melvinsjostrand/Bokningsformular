8134c0789597d89a8a97be03316b279fef7adb0a
melvinsjostrand <mesj04001@utb.vaxjo.se>
2023-10-18 13:33:45 +0200

    fixade så mindre variabler

--- script.js
+++ script.js
@@ -1,127 +1,123 @@
-
-
-let roomType;
-let lakeview;
-let form; 
+// Deklarera globala variabler
+let form;
+let totalCost;
 let customer;
 let campaignCodeValue = 1;
 
-function init(){
+// Funktion som körs när sidan laddas
+function init() {
+    // Hitta formuläret och lakeview-elementet i DOM
     form = document.querySelector("form");
-    lakeview = form.elements.addition[2];
-    console.log(form);
+    totalCost = document.getElementById("totalCost");
+    totalCost.innerHTML = "600";
 
-    for(i = 0; i<form.elements.roomType.length; i++){
-        form.elements.roomType[i].addEventListener("click" , event =>{
+    // Lägg till händelselyssnare för klick på rumstyp och tillägg
+    for (let i = 0; i < form.elements.roomType.length; i++) {
+        form.elements.roomType[i].addEventListener("click", (event) => {
             checkIfFamilyRoom();
             cost();
-        })
+        });
     }
-    for(i = 0; i<form.elements.addition.length; i++){
-        form.elements.addition[i].addEventListener("click" , event =>{
+    for (let i = 0; i < form.elements.addition.length; i++) {
+        form.elements.addition[i].addEventListener("click", (event) => {
             checkIfFamilyRoom();
             cost();
-        })
+        });
     }
-    form.elements.nights.addEventListener("click" , event =>{
+
+    // Lägg till händelselyssnare för antal nätter och kampanjkod
+    form.elements.nights.addEventListener("click", (event) => {
         cost();
-    })
-    form.elements.campaigncode.addEventListener("keyup" , event =>{
+    });
+    form.elements.campaigncode.addEventListener("keyup", (event) => {
         campaignCode();
         cost();
-    })
+    });
 
-    form.elements.telephone.addEventListener("blur" , event =>{
+    // Lägg till händelselyssnare för telefonnummer, postnummer och stad
+    form.elements.telephone.addEventListener("blur", (event) => {
         phoneNumber();
-    })
-    form.elements.zipcode.addEventListener("blur" , event =>{
+    });
+    form.elements.zipcode.addEventListener("blur", (event) => {
         zipCode();
-    })
-    form.elements.city.addEventListener("blur" , event =>{
+    });
+    form.elements.city.addEventListener("blur", (event) => {
         locality();
-    })
+    });
+
+    // Kontrollera om det är en familjerum
     checkIfFamilyRoom();
 }
+
+// Kör init-funktionen när sidan laddas
 window.onload = init;
 
-
-function checkIfFamilyRoom(){
-    let persons = form.elements.persons;
+// Funktion för att kontrollera om det är ett familjerum
+function checkIfFamilyRoom() {
     if (form.elements.roomType[2].checked === true) {
-        lakeview.disabled = true;  
-        lakeview.parentNode.style.color = "#000";
-        persons.disabled = false;
-    }else{
-        lakeview.disabled = false;
-        persons.disabled = true;
-        persons.parentNode.style.color = "#000";
+        form.elements.addition[2].disabled = true;
+        form.elements.addition[2].parentNode.style.color = "#000";
+        form.elements.persons.disabled = false;
+    } else {
+        form.elements.addition[2].disabled = false;
+        form.elements.persons.disabled = true;
+        form.elements.persons.parentNode.style.color = "#000";
     }
 }
 
-function cost(){
+// Funktion för att beräkna kostnaden
+function cost() {
     let nights = Number(form.elements.nights.value);
     let roomCost = Number(form.elements.roomType.value.split(",")[1]);
-    let totalCost = document.getElementById("totalCost");
-    for(i = 0; i < form.elements.addition.length; i++){
-        if(form.elements.addition[i].checked && !form.elements.addition[i].disabled){
-            roomCost+=Number(form.elements.addition[i].value.split(",")[1]);
+    totalCost = document.getElementById("totalCost");
+    for (let i = 0; i < form.elements.addition.length; i++) {
+        if (form.elements.addition[i].checked && !form.elements.addition[i].disabled) {
+            roomCost += Number(form.elements.addition[i].value.split(",")[1]);
         }
     }
-    totalCost.innerHTML = (roomCost * nights)  * campaignCodeValue;
-    console.log("värdet på allt är " + (roomCost * nights)  * campaignCodeValue + "Kr");
-}
-//skapa en till if else sats för att vilka som är i checkade
-function additionCost(){
-    let AdditionCost = Number(form.elements.addition.value);
-    if(form.elements.addition[0].checked == true){
-        AdditionCost[0]
-    }else if(form.elements.addition[1].checked == true){
-        AdditionCost[1]
-    }else if(form.elements.addition[2].checked == true){
-        AdditionCost[2]
-    }
+    totalCost.innerHTML = (roomCost * nights) * campaignCodeValue;
+    console.log("värdet på allt är " + (roomCost * nights) * campaignCodeValue + "Kr");
 }
 
-function phoneNumber(){
+// Funktion för att validera telefonnummer
+function phoneNumber() {
     const regexPhone = /^0[0-9-/ ]{6,14}$/;
     customer = form.elements.telephone.parentNode.parentNode.getElementsByTagName("span")[1];
-    let ok = regexPhone.exec(form.elements.telephone.value);
-    if(!ok){
+    if (!regexPhone.exec(form.elements.telephone.value)) {
         customer.innerHTML = "Du måste skriva 6 till 14 tecken och börja med 0";
-        customer.parentNode.style.color = "#ff0000"; 
-    }else{
+        customer.parentNode.style.color = "#ff0000";
+    } else {
         customer.innerHTML = "";
     }
 }
 
-function zipCode(){
+// Funktion för att validera postnummer
+function zipCode() {
     const regexZip = /[0-9]{5}$/;
     customer = form.elements.zipcode.parentNode.parentNode.getElementsByTagName("span")[1];
-    let ok = regexZip.exec(form.elements.zipcode.value);
-   
-    if(!ok){
-        customer.innerHTML = "Ditt postnummer måste får endast innehålla siffror och har 5 siffror";
-        customer.parentNode.style.color = "#ff0000"; 
-    }else{
+    if (!regexZip.exec(form.elements.zipcode.value)) {
+        customer.innerHTML = "Ditt postnummer måste får endast innehålla siffror och ha 5 siffror";
+        customer.parentNode.style.color = "#ff0000";
+    } else {
         customer.innerHTML = "";
     }
 }
-//funktioner för validering av input fält   
-function campaignCode(){
+
+// Funktion för att hantera kampanjkod
+function campaignCode() {
     const regexCampaign = /[A-Za-z]{3}-[0-9]{2}-[A-Za-z]{1}[0-9]{1}$/;
-    let ok = regexCampaign.exec(form.elements.campaigncode.value);
-    if(ok){
-        form.elements.campaigncode.style.backgroundColor  = "#99ff99";
+    if (regexCampaign.exec(form.elements.campaigncode.value)) {
+        form.elements.campaigncode.style.backgroundColor = "#99ff99";
         campaignCodeValue = 0.95;
-    }else{
-        form.elements.campaigncode.style.backgroundColor  = "#ff0000";
+    } else {
+        form.elements.campaigncode.style.backgroundColor = "#ff0000";
         campaignCodeValue = 1;
     }
 }
 
-function locality(){
+// Funktion för att omvandla stad till stora bokstäver
+function locality() {
     let text = form.elements.city.value;
     text = text.toUpperCase();
     form.elements.city.value = text;
 }
-
